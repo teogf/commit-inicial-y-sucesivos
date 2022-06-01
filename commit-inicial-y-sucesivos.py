42425363d5f61dac8c19ca8b7f676ff7ac1326e9
Teodoro <teodoro@refritrans.local>
2022-06-01 10:00:29 +0200

    first commit

---
+++ commit-inicial-y-sucesivos.py
@@ -0,0 +1,14 @@
+# -- COMMIT INICIAL 
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/teogf/"repositorio con el nombre que sea".git
+git push -u origin main
+
+# -- COMMIT SUCESIVOS
+git init
+git status
+git add .
+git commit -m "mensaje"
+git push
